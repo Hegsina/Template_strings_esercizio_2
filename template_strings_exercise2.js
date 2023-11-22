bdd9585ec08ed0633700fef601532e26212320ca
Hegsina <hegelminniti@gmail.com>
2023-11-22 13:54:30 +0100

    Template Strings - Exercise 2

---
+++ template_strings_exercise2.js
@@ -0,0 +1,9 @@
+//Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+let aCapo = `Questa è 
+una stringa
+su più righe
+che include
+delle interruzioni.`;
+
+console.log(aCapo);
